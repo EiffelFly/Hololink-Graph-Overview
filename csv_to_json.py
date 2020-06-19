0f6d51576c1e19263c472e0d68584f3592eec3ec
EiffelFly <eric525282@gmail.com>
2020-06-19 22:54:34 +0800

    操作d3-force，修改csvToJson: 增加 level: media, article, tag

--- csv_to_json.py
+++ csv_to_json.py
@@ -10,12 +10,15 @@ linkvalidator = []
 data = pd.read_csv('Hololink_Tags_chinaIndia.csv')
 
 for i,j,k,z in zip(data['title'], data['media'], data['url'],data['tags']):
+
+    tagAmount = 0
+
     if i not in nodevalidator:
-        nodejson.append({"id":f"{i}", "url":f"{k}"})
+        nodejson.append({"id":f"{i}", "url":f"{k}", "level":"article", "tagAmount":0})
         nodevalidator.append(i)
 
     if j not in nodevalidator:
-        nodejson.append({"media":f"{j}", "publish":0})
+        nodejson.append({"id":f"{j}","level":"media", "publish":0})
         nodevalidator.append(j)
 
     tags = z.split(',')
@@ -24,7 +27,7 @@ for i,j,k,z in zip(data['title'], data['media'], data['url'],data['tags']):
     for tag in tags:
         tag = tag.strip()
         if tag not in nodevalidator:
-            nodejson.append({"id":f"{tag}", "connection":0})
+            nodejson.append({"id":f"{tag}","level":"tag" ,"connection":0})
             nodevalidator.append(tag)
         
         for node in nodejson:
@@ -36,15 +39,25 @@ for i,j,k,z in zip(data['title'], data['media'], data['url'],data['tags']):
                 detail = e.args[0] 
                 #print(error_class, detail)
 
+        #計算 article tag 數量
+        tagAmount += 1
+
+
+
         linkjson.append({"source":f"{i}", "target":f"{tag}"})
 
     #計算 media public 數量
     for node in nodejson:
         try:
-            if node['media'] == j:
-                node['publish'] += 1
+            if node['level'] == "media" and node['id'] == j:
+                node["publish"] += 1
+            if node['id'] == i:
+                node["tagAmount"] = tagAmount
         except:
             pass
+
+    
+
 
 dataJson = {
     "nodes":nodejson,
@@ -52,5 +65,5 @@ dataJson = {
 }
 
 exportJson = json.dumps(dataJson,ensure_ascii=False)
-with open('China_India.json', 'w') as file:
+with open('China_India.json', 'w', encoding='utf-8') as file:
     file.write(exportJson)
