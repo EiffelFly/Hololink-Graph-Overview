20fb02a806d581c2f03175a022288a023c72a582
eiffelfly <eric525282@gmail.com>
2020-07-29 11:31:53 +0800

    Major - append countLinksTargetOwn parameter in linkjson

--- csv_to_json.py
+++ csv_to_json.py
@@ -44,7 +44,7 @@ for i,j,k,z in zip(data['title'], data['media'], data['url'],data['tags']):
 
 
 
-        linkjson.append({"source":f"{i}", "target":f"{tag}"})
+        linkjson.append({"source":f"{i}", "target":f"{tag}", "countLinksTargetOwn":""})
 
     #計算 media publish 數量
     for node in nodejson:
@@ -53,17 +53,24 @@ for i,j,k,z in zip(data['title'], data['media'], data['url'],data['tags']):
                 node["publish"] += 1
             if node['id'] == i:
                 node["tagAmount"] = tagAmount
+
+            
         except:
             pass
 
-    
-
-
+#Count Amount of Links the target node own and put it into links
+for link in linkjson:
+    targettag = link['target']
+    for node in nodejson:
+        if node['level'] == "tag":
+            if node['id'] == targettag:
+                link['countLinksTargetOwn'] = node['connection']
+                
 dataJson = {
     "nodes":nodejson,
     "links":linkjson
 }
 
 exportJson = json.dumps(dataJson,ensure_ascii=False)
-with open('China_India.json', 'w', encoding='utf-8') as file:
+with open('China_India_test.json', 'w', encoding='utf-8') as file:
     file.write(exportJson)
